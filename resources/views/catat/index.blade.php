bf84b1e83b8a72dc7e30346f8f6e253582242b70
irfannur238 <irfannur238@gmail.com>
2022-05-28 21:09:39 +0700

    add js destroy interbal timer

--- resources/views/catat/index.blade.php
+++ resources/views/catat/index.blade.php
@@ -2,7 +2,7 @@
 @extends('../layout/main')
 
 <!-- isi bagian judul halaman -->
-<!-- cara penulisan isi section yang pendek -->
+<!-- cara penulisan isi section yang pendek ryrt-->
 @section('judul_halaman', 'Halaman Home')
 
 
@@ -10,81 +10,83 @@
 <!-- cara penulisan isi section yang panjang -->
 @section('konten')
 
-    <div class="row align-items-md-stretch">
-    <div class="col-md-6">
-        <div class="h-100 p-5 bg-light border rounded-3">
-          <h2>Select Your Time</h2>
+<div class="row align-items-md-stretch">
+  <div class="col-md-6">
+    <div class="h-100 p-5 bg-light border rounded-3">
+      <h2>Select Your Time</h2>
 
-          <form class="row g-3">
-  <div class="col-12">
-  <select class="form-select" aria-label="Default select example">
-            <option selected>09:00</option>
-            <option value="1">08:00</option>
-            <option value="2">12:00</option>
-            <option value="3">01:00</option>
+      <form class="row g-3">
+        <div class="col-12">
+          <select class="form-select sel-dur" aria-label="Default select
+            example">
+            <option value="540">09:00</option>
+            <option value="480">08:00</option>
+            <option value="720">12:00</option>
+            <option value="60">01:00</option>
           </select>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
+        </div>
+        <div class="col-12">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck">
+            <label class="form-check-label" for="gridCheck">
+              Check me out
+            </label>
+          </div>
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-outline-secondary">Submit</button>
+        </div>
+      </form>
+
+
     </div>
   </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-outline-secondary">Submit</button>
-  </div>
-</form>
-          
-         
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="h-100 p-5 text-white bg-dark rounded-3">
-          <h2>Time Is Money</h2>
-          <h1 id="time">...</h1>
-          <p>Or, keep it light and add a border for some added definition to the boundaries of your content</p>
-        </div>
-      </div>
-      
+  <div class="col-md-6">
+    <div class="h-100 p-5 text-white bg-dark rounded-3">
+      <h2>Time Is Money</h2>
+      <h1 id="time">...</h1>
+      <p>Or, keep it light and add a border for some added definition to the
+        boundaries of your content</p>
     </div>
+  </div>
 
-    <hr>
-
-    <div class="p-5 mb-4 border bg-light rounded-3">
-    <table class="table table-striped table-hover">
-    <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
 </div>
- 
-	<!-- <p>Nama : {{ $name }}</p>
+
+<hr>
+
+<div class="p-5 mb-4 border bg-light rounded-3">
+  <table class="table table-striped table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<!-- <p>Nama : {{ $name }}</p>
  
 	<p>Mata Pelajaran</p>
 	<ul>
@@ -96,17 +98,38 @@
 		@endforeach
 		
 	</ul> -->
-
-@endsection
-
 <!-- <div>Registration closes in <span id="time">05:00</span> minutes!</div> -->
-
 <script>
-    console.log('dad');
+    //console.log('dad');
 
-	function startTimer(duration, display) {
+	
+
+// window.onload = function () {
+//     var fiveMinutes = 60*59;
+//     //     display = document.querySelector('#time');
+//     // startTimer(fiveMinutes, display);
+//     getTime(fiveMinutes);
+// };
+
+
+$('.sel-dur').on('change', function() {
+    var dur = $(this).val();
+    console.log(dur);
+    getTime(dur);
+});
+
+var prevNowPlaying = null;
+
+function getTime(duration) {
+
+    if (prevNowPlaying) {
+        clearInterval(prevNowPlaying);
+    }
+
+    display = document.querySelector('#time');
     var timer = duration, minutes, seconds;
-    setInterval(function () {
+
+    prevNowPlaying = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -115,21 +138,17 @@
 
 				var res = minutes + ":" + seconds;
        
-
         if (--timer < 0) {
             //timer = duration;
-            var ex = 
-            res = "-" + res.replace('-','');
+            // var ex = 
+            // res = "-" + res.replace('-','');
             //console.log('ddd');
         }
         
-         display.textContent = res;
+        display.textContent = res;
     }, 1000);
-}
 
-window.onload = function () {
-    var fiveMinutes = 60*59,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+}
 </script>
+
+@endsection
